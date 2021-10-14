d3ac3f800118c39a9913ec6aea9476b2b351a786
Rupali Vyas <vyas.rupali655@gmail.com>
2021-10-14 13:17:58 +0530

    Mistake Corrected

--- src/main/java/FourBallsProcedural.java
+++ src/main/java/FourBallsProcedural.java
@@ -6,6 +6,7 @@ public class FourBallsProcedural extends PApplet {
     int b2 = 0;
     int b3 = 0;
     int b4 = 0;
+    int x = 0;
 
     public static void main(String[] args) {
 
@@ -21,14 +22,31 @@ public class FourBallsProcedural extends PApplet {
         size(width, height);
     }
 
-    @Override
-    public void draw() {
+    public void draw(){
+        firstBall();
+        secondBall();
+        thirdBall();
+        fourthBall();
+    }
+
+
+
+    private void firstBall() {
         ellipse(b1, 1*height/5, 10, 10);
         b1++;
+    }
+
+    private void secondBall() {
         ellipse(b2, 2*height/5, 10, 10);
         b2+=2;
+    }
+
+    private void thirdBall() {
         ellipse(b3, 3*height/5, 10, 10);
         b3+=3;
+    }
+
+    private void fourthBall() {
         ellipse(b4, 4*height/5, 10, 10);
         b4+=4;
     }
